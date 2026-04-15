c37dce3dd3e89879f742767aedc25392dd8b2810
mafaqaslam <aslammuhammadafaq@gmail.com>
2026-04-15 02:53:57 -0600

    Add files via upload

---
+++ Text reference files/reference video data.js
@@ -0,0 +1,12 @@
+const videosData = [
+  {
+    id: "my-first-video",
+    youtubeId: "YOUR_VIDEO_ID_HERE",
+    title: "Your Video Title",
+    category: "RESERVOIR ENGINEERING",
+    duration: "14:32",
+    description: "Your video description here...",
+    date: "April 14, 2026",
+    views: 0
+  }
+];
